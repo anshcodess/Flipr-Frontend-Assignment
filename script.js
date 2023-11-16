5f65470e82f80d03ba2967f5ba77669c471eaf8c
anshcodess <justforfun0469@gmail.com>
2023-11-16 22:23:21 +0530

    added show all data button

--- script.js
+++ script.js
@@ -4,68 +4,73 @@ const year = document.getElementById("year");
 
 // Below function is used to Dynamically populate Year in DropDownList
 window.onload = function () {
-    //Reference the DropDownList.
-    var year = document.getElementById("year");
+  //Reference the DropDownList.
+  var year = document.getElementById("year");
 
-    //Determine the Current Year.
-    // var currentYear = (new Date()).getFullYear();
+  //Determine the Current Year.
+  // var currentYear = (new Date()).getFullYear();
 
-    //Loop and add the Year values to DropDownList.
-    for (var i = 2018; i >= 1901; i--) {
-        var option = document.createElement("OPTION");
-        option.innerHTML = i;
-        option.value = i;
-        year.appendChild(option);
-    }
+  //Loop and add the Year values to DropDownList.
+  for (var i = 2018; i >= 1901; i--) {
+    var option = document.createElement("OPTION");
+    option.innerHTML = i;
+    option.value = i;
+    year.appendChild(option);
+  }
 };
 
 const getData = async () => {
-    const res = await fetch("https://api.nobelprize.org/v1/prize.json");
-    const data = await res.json();
-    return data;
-}
+  const res = await fetch("https://api.nobelprize.org/v1/prize.json");
+  const data = await res.json();
+  return data;
+};
 
 const displayWinners = async () => {
+  let categoryVal = category.value;
+  let yearVal = year.value;
 
-    let categoryVal = category.value;
-    let yearVal = year.value;
+  const payload = await getData();
 
-    const payload = await getData();
+  let dataDisplayObj = payload.prizes.filter((eventData) => {
+    if (eventData.category == categoryVal && eventData.year == yearVal) {
+      return eventData;
+    }
+  });
 
-    let dataDisplayObj = payload.prizes.filter((eventData) => {
-        if(eventData.category == categoryVal && eventData.year == yearVal){
-            return eventData;
-        }
-    });
-
-    let dataDisplay = dataDisplayObj[0].laureates.map((object) => {
-
-        return `
+  let dataDisplay = dataDisplayObj[0].laureates
+    .map((object) => {
+      return `
         <div class="winnerDetails-container">
             <p>Name: ${object.firstname} ${object.surname}</p>
             <p>Motivation: ${object.motivation}</p>
         </div>
-        `
+        `;
+    })
+    .join("");
 
-    }).join("");
-
-    dispaly.innerHTML = dataDisplay;
-
-}
+  dispaly.innerHTML = dataDisplay;
+};
 
 const allLaureates = async () => {
 
-    const payload = await getData();
+  const payload = await getData();
 
-    let dataDisplay = payload.prizes.laureates.map((object) => {
+  dispaly.innerHTML = [];
 
+  for (var i = 0; i < payload.prizes.length; i++) {
+    let dataDisplay = payload.prizes[i].laureates
+      .map((object) => {
         return `
-        <div class="winnerDetails-container">
-            <p>Name: ${object.firstname} ${object.surname}</p>
-            <p>Motivation: ${object.motivation}</p>
-        </div>
-        `
+            <div class="winnerDetails-container">
+                <p>Category: ${payload.prizes[i].category}</p>
+                <p>Year: ${payload.prizes[i].year}</p>
+                <p>Name: ${object.firstname} ${object.surname}</p>
+                <p>Motivation: ${object.motivation}</p>
+            </div>
+            `;
+      })
+      .join("");
 
-    }).join("");
-
-}
+    dispaly.innerHTML += dataDisplay;
+  }
+};
